1d6f629d4d5b93f236c87de00ab01bdac822fcd1
Pluents <227794090@qq.com>
2025-11-17 12:54:54 +0800

    The Base Character Class

---
+++ Source/GAS_Aura/Public/Character/BaseCharacter.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BaseCharacter.generated.h"
+
+UCLASS()
+class GAS_AURA_API ABaseCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ABaseCharacter();
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	
+
+};

e2c0a778900c14d12bebe5636c1833a553e0e39b
Pluents <227794090@qq.com>
2025-11-23 17:47:37 +0800

    2025/11/23

--- Source/GAS_Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/GAS_Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -13,6 +13,39 @@
 	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
 	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
+USTRUCT()
+struct FEffectProperties
+{
+	GENERATED_BODY()
+	FEffectProperties(){}
+	
+	FGameplayEffectContextHandle EffectContextHandle;
+	
+	UPROPERTY()
+	UAbilitySystemComponent*SourceASC=nullptr;
+	
+	UPROPERTY()
+	AActor*SourceAvatarActor=nullptr;
+	
+	UPROPERTY()
+	AController*SourceController=nullptr;
+	
+	UPROPERTY()
+	ACharacter*SourceCharacter=nullptr;
+	
+	
+	UPROPERTY()
+	UAbilitySystemComponent*TargetASC=nullptr;
+	
+	UPROPERTY()
+	AActor*TargetAvatarActor=nullptr;
+	
+	UPROPERTY()
+	AController*TargetController=nullptr;
+	
+	UPROPERTY()
+	ACharacter*TargetCharacter=nullptr;
+};
 /**
  * 
  */
@@ -24,6 +57,10 @@ class GAS_AURA_API UAuraAttributeSet : public UAttributeSet
 public:
 	UAuraAttributeSet();
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	
+	virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
 	
 	//健康
 	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_Health,Category="Vital Attributes")
@@ -53,4 +90,12 @@ public:
 	void OnRep_Mana(const FGameplayAttributeData& OldMana)const;
 	UFUNCTION()
 	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana)const;
+	
+private:
+	static void SetEffectProperties(const FGameplayEffectModCallbackData& Data,FEffectProperties&Props);
 };
+
+
+
+
+
